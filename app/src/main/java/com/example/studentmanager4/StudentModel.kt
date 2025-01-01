ed7f38ca7ca6804658bd58a411904b6458140988
Thien021103 <thien881199@gmail.com>
2025-01-01 23:27:07 +0700

    first

---
+++ app/src/main/java/com/example/studentmanager4/StudentModel.kt
@@ -0,0 +1,12 @@
+package com.example.studentmanager4
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "students")
+data class StudentModel(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    var studentName: String,
+    var studentId: String
+) : Serializable
